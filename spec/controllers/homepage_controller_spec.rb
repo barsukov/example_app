04e5cdc6c70b580a4cd3bd05ca29b944de472aa7
Vladimir Barsukov <vladimir.barsukov@infopark.de>
2014-11-25 17:43:06 +0100

    update format commit

--- spec/controllers/homepage_controller_spec.rb
+++ spec/controllers/homepage_controller_spec.rb
@@ -13,7 +13,7 @@ describe HomepageController do
 
  	context "failed authenticate without credentials" do 
 	  it 'responds with 401' do  
- 	  	controller.request.env['HTTP_AUTHORIZATION'] = nil
+ 	    controller.request.env['HTTP_AUTHORIZATION'] = nil
 	  	get :index
 	  	expect(response).to have_http_status(:unauthorized)  
 	  end
